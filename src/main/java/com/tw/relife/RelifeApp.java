ecd2fa930195a19486f6f786d9d6c2b3e7e0d22d
Kuan Xin <kxin@thoughtworks.com>
2018-09-27 16:30:51 +0800

    [kxin]:refactor(RelifeMvcHandlerBuilder):pass should_be_register_mapping_mix_controller_and_action case

--- src/main/java/com/tw/relife/RelifeApp.java
+++ src/main/java/com/tw/relife/RelifeApp.java
@@ -24,6 +24,7 @@ public class RelifeApp implements RelifeAppHandler {
                 Throwable exception = ((InvocationTargetException) e).getTargetException();
                 return handleException(exception);
             }
+
             return handleException(e);
         }
     }
--- src/test/java/com/tw/relife/RelifeControllerTest.java
+++ src/test/java/com/tw/relife/RelifeControllerTest.java
@@ -170,4 +170,46 @@ class RelifeControllerTest {
 
         assertEquals(500,response.getStatus());
     }
+
+
+    @Test
+    void should_be_register_mapping_mix_controller_and_action() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        RelifeAppHandler handler = new RelifeMvcHandlerBuilder()
+                .addController(ExceptionTestController.class)
+                .addAction("/path", RelifeMethod.GET, request -> new RelifeResponse(200,"Hello","text/plain"))
+                .addAction("/student", RelifeMethod.GET, request -> new RelifeResponse(200,"Student","text/plain"))
+                .addAction("/badRequest",RelifeMethod.GET,request -> {throw new SampleNotFoundException();})
+                .build();
+
+        RelifeApp app = new RelifeApp(handler);
+        RelifeResponse badRequest = app.process(
+                new RelifeRequest("/badRequest", RelifeMethod.GET));
+
+        assertEquals(400,badRequest.getStatus());
+
+        RelifeResponse notFound = app.process(
+                new RelifeRequest("/notFound", RelifeMethod.GET));
+
+        assertEquals(404,notFound.getStatus());
+
+        RelifeResponse response = app.process(
+                new RelifeRequest("/normalException", RelifeMethod.GET));
+
+        assertEquals(500,response.getStatus());
+
+        RelifeResponse studentResponse = app.process(
+                new RelifeRequest("/student", RelifeMethod.GET));
+
+        assertEquals(200,studentResponse.getStatus());
+        assertEquals("Student",studentResponse.getContent());
+        assertEquals("text/plain",studentResponse.getContentType());
+
+        response = app.process(
+                new RelifeRequest("/path", RelifeMethod.GET));
+
+        assertEquals(200,response.getStatus());
+        assertEquals("Hello",response.getContent());
+        assertEquals("text/plain",response.getContentType());
+
+    }
 }
